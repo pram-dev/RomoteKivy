eb9e61adbff5b863f9a2c006e32e2fa98bce73a2
Ramos <notpramdev@gmail.com>
2022-09-10 12:42:23 -0700

    implemented kivymd properties

--- main.py
+++ main.py
@@ -3,26 +3,41 @@ GUI implementation of romote.py
 """
 
 import kivy
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
 
 kivy.require("2.1.0")
 
 
-class AppRootLayout(GridLayout):
+class SettingsAboutScreen(MDScreen):
+    """
+    Screen containing settings and 'about' section
+    """
+    pass
+
+
+class MainRemoteScreen(MDScreen):
+    """
+    Main/default screen containing remote
+    """
+    pass
+
+
+class RootScreenManager(MDScreenManager):
     """
     Main Layout for main screen
     """
     pass
 
 
-class RomoteApp(App):
+class RomoteApp(MDApp):
     """
     Base Romote app
     """
 
     def build(self):
-        return AppRootLayout()
+        return RootScreenManager()
 
 
 if __name__ == "__main__":
