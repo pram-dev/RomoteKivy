44a0bd0f9a0b25e46b2fd5728025f9283dd883f6
Ramos <notpramdev@gmail.com>
2022-09-10 19:39:50 -0700

    implementing Romote wrapper for Roku class

--- romote.py
+++ romote.py
@@ -24,22 +24,27 @@ class Romote(Roku):
                 success = False
 
             return success
-
         return safe_command_func
 
     def attempt_first_contact(self):
-        test_command = Romote.safe_command_wrapper(self.up)
-        success = test_command()
+        success = self.up()
         if success:
             self.CONTACT_ESTABLISHED = True
 
     def __init__(self, host=None, *args, **kwargs):
+        self.safe_wrap_all_commands(Romote.safe_command_wrapper)
 
         if host:
             super().__init__(host, *args, **kwargs)
             self.attempt_first_contact()
 
-    def attempt_contact(ip_str):
-        pass
+    def safe_wrap_all_commands(self, wrapper_func):
+
+        self.up = wrapper_func(self.up)
+        self.right = wrapper_func(self.right)
+        self.down = wrapper_func(self.down)
+        self.left = wrapper_func(self.left)
+        self.select = wrapper_func(self.select)
+        self.back = wrapper_func(self.back)
 
     CONTACT_ESTABLISHED = False
