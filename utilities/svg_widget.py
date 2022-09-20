e3e9eb5373d70af1f88b8f53ee5c03e10a5abf5d
Ramos <notpramdev@gmail.com>
2022-09-20 00:57:22 -0700

    removed code, leaving only the svg-widget class

--- utilities/svg_widget.py
+++ utilities/svg_widget.py
@@ -1,11 +1,6 @@
-import sys
-from glob import glob
-from os.path import join, dirname
+
 from kivy.uix.scatter import Scatter
-from kivy.app import App
 from kivy.graphics.svg import Svg
-from kivy.core.window import Window
-from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 
 Builder.load_string("""
@@ -28,23 +23,3 @@ class SvgWidget(Scatter):
         with self.canvas:
             svg = Svg(filename)
         self.size = svg.width, svg.height
-
-
-class SvgApp(App):
-
-    def build(self):
-        self.root = FloatLayout()
-
-        filenames = sys.argv[1:]
-        if not filenames:
-            filenames = glob(join(dirname(__file__), '*.svg'))
-
-        for filename in filenames:
-            svg = SvgWidget(filename, size_hint=(None, None))
-            self.root.add_widget(svg)
-            svg.scale = 5.
-            svg.center = Window.center
-
-
-if __name__ == '__main__':
-    SvgApp().run()
