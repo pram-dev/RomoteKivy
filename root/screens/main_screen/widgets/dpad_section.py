749a3295455adeb28bd66ce29680c70c8eb73dc0
Ramos <notpramdev@gmail.com>
2022-09-20 06:59:27 -0700

    working on dpad button widget

--- root/screens/main_screen/widgets/dpad_section.py
+++ root/screens/main_screen/widgets/dpad_section.py
@@ -1,17 +1,19 @@
 from os.path import join
-from kivymd.uix.widget import MDWidget
 from kivymd.uix.relativelayout import MDRelativeLayout
+from kivy.uix.scatter import Scatter
 from kivy.graphics.svg import Svg
 
 
-class DPadSVG(MDWidget):
+class DPadButton(Scatter):
 
-    def __init__(self, filename=None, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         filename = join("svg", "dpad.svg")
         with self.canvas:
+            self.anchor_x = "center"
+            self.anchor_y = "center"
             self.svg = Svg(filename)
+            self.scale = 1.5
         self.size = self.svg.width, self.svg.height
 
 
