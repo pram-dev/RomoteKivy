30857d05c7130996083fa0c3ba207bd3d7bd76ef
Ramos <notpramdev@gmail.com>
2022-09-20 04:59:24 -0700

    updated svg dpad

--- root/screens/main_screen/widgets/dpad_section.py
+++ root/screens/main_screen/widgets/dpad_section.py
@@ -3,4 +3,10 @@ from kivymd.uix.anchorlayout import MDAnchorLayout
 
 
 class ControllerDPadSection(MDAnchorLayout):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        dpad = SvgWidget("svg/dpad.svg")
+        dpad.scale = 1.5
+        self.add_widget(dpad)
