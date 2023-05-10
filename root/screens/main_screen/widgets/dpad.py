752f161b82860838bccad3b8adafcaca3871638f
development <development@ArchBox.parrot>
2023-05-10 01:12:13 -0700

    Include dpad kv file

---
+++ root/screens/main_screen/widgets/dpad.py
@@ -0,0 +1,11 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.button import MDIconButton, MDFloatingActionButton
+
+class DirectionalPad(BoxLayout):
+    def direction(self, direction):
+        # Do something with the direction button press
+        pass
+
+    def select(self):
+        # Do something with the select button press
+        pass
